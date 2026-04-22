f6d5b4b33c3c1bbd8106c9ac7b1e29d1faa8ccb1
chandamin <chandamin084@gmail.com>
2026-04-22 10:24:57 +0530

    subscription payload change

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 module.exports = async function connectDB() {
-    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     
 };
